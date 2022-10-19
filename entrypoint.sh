588447bbd13e791e7972f7b7a768501426bd22ec
SitoRBJ <sitoruizbravo@hotmail.com>
2022-10-19 11:54:04 +0200

    temporal docker dir review

--- entrypoint.sh
+++ entrypoint.sh
@@ -82,6 +82,8 @@ while getopts "a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:p:q:r:s:t:u:v:w:x:y:" o; do
   esac
 done
 
+echo "Review dir"
+ls -la
 echo "Starting security checks"
 
 if [[ ${DTRACK_ENABLE} == *"true"* ]]; then
