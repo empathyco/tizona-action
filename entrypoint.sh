297026f8c70eb021fac4f039365decc28e41094c
SitoRBJ <sitoruizbravo@hotmail.com>
2022-10-19 12:06:40 +0200

    Temporal find

--- entrypoint.sh
+++ entrypoint.sh
@@ -83,7 +83,9 @@ while getopts "a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:p:q:r:s:t:u:v:w:x:y:" o; do
 done
 
 echo "Review dir"
-ls -la
+pwd
+find .  -name code.sh
+find /  -name code.sh
 echo "Starting security checks"
 
 if [[ ${DTRACK_ENABLE} == *"true"* ]]; then
