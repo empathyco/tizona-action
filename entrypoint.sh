f94f5f0b1aefaa7d31ad654b133720d9746cfaaa
SitoRBJ <sitoruizbravo@hotmail.com>
2022-11-02 11:32:51 +0100

    Improve wait and error management

--- entrypoint.sh
+++ entrypoint.sh
@@ -284,6 +284,20 @@ else
     echo "TIZONA: Skip secrets leaks action"
 fi
 
-wait || { echo "TIZONA: background check failed: $?" >&2; exit 1;}
+declare -i err=0 werr=0
+while wait -fn || werr=$?; ((werr != 127)); do
+  err=$werr
+  ## To handle *as soon as* first failure happens:
+  ((err == 0)) || break
+done
+## If you want to still wait for children to finish before exiting
+## parent (even if you handle the failed child early) uncomment this:
+#trap 'wait || :' EXIT
+if ((err == 0)); then
+  echo "TIZONA: Success"
+else
+  echo "TIZONA: Failure! At least one error found."
+  exit $err
+fi
 
 echo "TIZONA: Security checks finished"
