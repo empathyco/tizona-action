98610dc277b2e69fd20a1d865c0f47841ec490b7
SitoRBJ <sitoruizbravo@hotmail.com>
2022-11-08 11:34:48 +0100

    Improve logs

--- tfsec_check.sh
+++ tfsec_check.sh
@@ -16,9 +16,10 @@ set -Eeuo pipefail
 cd "${GITHUB_WORKSPACE}/${REVIEWDOG_DIR}" || { echo "TIZONA - Tfsec review: Terraform directory not found, skipping." ; exit 1;}
 
 echo "TIZONA - Tfsec review: Print tfsec details ..."
-tfsec --version
+tfsec_v= `tfsec --version`
+echo "TIZONA - Tfsec review: Tfsec version: ${tfsec_v}"
 
-echo 'Running tfsec with reviewdog ...'
+echo 'TIZONA - Tfsec review: Running tfsec with reviewdog ...'
 export REVIEWDOG_GITHUB_API_TOKEN="${REVIEWDOG_GIT_TOKEN}"
 
 # Allow failures now, as reviewdog handles them
@@ -26,7 +27,7 @@ set +Eeuo pipefail
 
 # shellcheck disable=SC2086
 
-tfsec --format=json . | jq -r -f "/app/to-rdjson.jq" | reviewdog -f=rdjson -name="tfsec" -reporter="${REVIEWDOG_REPORTER}" -level="${REVIEWDOG_LVL}" -fail-on-error=true 
+tfsec --format=json --force-all-dirs . | jq -r -f "/app/to-rdjson.jq" | reviewdog -f=rdjson -name="tfsec" -reporter="${REVIEWDOG_REPORTER}" -level="${REVIEWDOG_LVL}" -fail-on-error=true 
 
 tfsec_return="${PIPESTATUS[0]}" reviewdog_return="${PIPESTATUS[2]}" exit_code=$?
 
