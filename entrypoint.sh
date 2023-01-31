3b6fa8005c8e076c580ecf5cc8057c96f9be071a
SitoRBJ <sitoruizbravo@hotmail.com>
2023-01-31 12:02:39 +0100

    Fix getopts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-while getopts "a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:p:q:r:s:t:u:v:w:x:y:z:A:B:C:D:E:F:G" o; do
+while getopts "a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:p:q:r:s:t:u:v:w:x:y:z:A:B:C:D:E:F:G:" o; do
    case "${o}" in
        a)
          export ACTION_MODE=${OPTARG}
