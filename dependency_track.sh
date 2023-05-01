bd3f5f81cfd2d202bc02012589189cfdd9d747ab
SitoRBJ <sitoruizbravo@hotmail.com>
2023-05-01 16:35:33 +0200

    Add SBT

--- dependency_track.sh
+++ dependency_track.sh
@@ -67,6 +67,31 @@ case $DTRACK_LANGUAGE in
         cd $GITHUB_WORKSPACE
         ;;
 
+    "scala-sbt")
+        cd $DTRACK_DIR
+        echo "TIZONA - Dependency Track: [*]  Processing Scaka SBT BoM"
+        echo "TIZONA - Dependency Track: [*]  Installing SBT"
+        apt-get update
+        apt-get install apt-transport-https curl gnupg -yqq
+        echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | tee /etc/apt/sources.list.d/sbt.list
+        echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | tee /etc/apt/sources.list.d/sbt_old.list
+        curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/scalasbt-release.gpg --import
+        chmod 644 /etc/apt/trusted.gpg.d/scalasbt-release.gpg
+        apt-get update
+        apt-get install sbt
+
+        path="$GITHUB_WORKSPACE/bom.xml"
+
+        echo "TIZONA - Dependency Track: [*]  SBT dependencyTree"
+        sbt dependencyTree
+        ;;
+
+    "scala-gradle")
+        cd $DTRACK_DIR
+        echo "TIZONA - Dependency Track: [*]  Processing Scaka GRADLE BoM"
+        
+        ;;
+
     "java")
         cd $DTRACK_DIR
         echo "TIZONA - Dependency Track: [*]  Processing Java BoM"
